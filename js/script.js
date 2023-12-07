2a7863c2ed8949a414b58848246dd28676691014
EleniVrabec <eleni.vrabec@gritacademy.se>
2023-12-07 09:29:40 +0100

    new

--- js/script.js
+++ js/script.js
@@ -104,20 +104,20 @@ const sections = document.querySelectorAll("section");
     ScrollReveal({ 
        // reset: true,
         distance:'60px',
-        duration:2500,
+        duration:1300,
         delay:100
     
     });
   
-    ScrollReveal().reveal('.home .info .h2, .section-title-01, .section-title-02', {delay:500, origin:'left' });
-    ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay:600, origin:'right' });
-    ScrollReveal().reveal('.home .info .btn-home', {delay:700, origin:'bottom' });
-    ScrollReveal().reveal('.media-icons i, .contact-left li', {delay:500, origin:'left', interval:200 });
-    ScrollReveal().reveal('.home-img, .about-img', {delay:500, origin:'bottom' });
-    ScrollReveal().reveal('.about .description, .contact-right', {delay:600, origin:'right' });
-    ScrollReveal().reveal('.skills-description, .contact-card, .contact-left h2 ', {delay:700, origin:'left'});
-    ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', {delay:800, origin:'bottom', interval:200});
-    ScrollReveal().reveal('footer .group', {delay:500, origin:'top', interval:200});
+    ScrollReveal().reveal('.home .info .h2, .section-title-01, .section-title-02', {delay:400, origin:'left' });
+    ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay:500, origin:'right' });
+    ScrollReveal().reveal('.home .info .btn-home', {delay:600, origin:'bottom' });
+    ScrollReveal().reveal('.media-icons i, .contact-left li', {delay:400, origin:'left', interval:200 });
+    ScrollReveal().reveal('.home-img, .about-img', {delay:400, origin:'bottom' });
+    ScrollReveal().reveal('.about .description, .contact-right', {delay:500, origin:'right' });
+    ScrollReveal().reveal('.skills-description, .contact-card, .contact-left h2 ', {delay:600, origin:'left'});
+    ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', {delay:700, origin:'bottom', interval:200});
+    ScrollReveal().reveal('footer .group', {delay:400, origin:'top', interval:200});
 
 
 
